0cec97fb6c039a22c8df18cbc464bc79fe9ecfc2
karthi <itsmecoder29@gmail.com>
2024-04-09 15:21:01 +0800

    32

---
+++ Hulk.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Hulk {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n==1){
+            System.out.println("I hate it");
+        }else{
+        for(int i=0;i<n;i++){
+            if(i==n-1 && i%2==0){System.out.print("I hate it");}
+            else if(i==n-1 && i%2!=0){System.out.print("I love it");}
+            else if(i%2==0){System.out.print("I hate that ");}
+            else{System.out.print("I love that ");}
+        }
+    }
+        sc.close();
+    }
+}
