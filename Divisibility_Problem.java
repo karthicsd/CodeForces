64bc5d78dad758be7cc25e66190cdbfde869897e
karthi <itsmecoder29@gmail.com>
2024-04-10 17:48:19 +0800

    34

---
+++ Divisibility_Problem.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Divisibility_Problem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++) {
+            int a = sc.nextInt(), b = sc.nextInt(), moves = 0;
+            if(a%b == 0){ System.out.println(0); }
+            else{
+               System.out.println(b-a%b);
+            }   
+        }
+        sc.close();
+    }
+}
