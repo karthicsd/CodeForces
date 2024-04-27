4442feed8296bf55d211a31f92f4999aee975c09
karthi <itsmecoder29@gmail.com>
2024-04-27 15:34:37 +0800

    59

---
+++ Again_Twenty_Five.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Again_Twenty_Five {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        System.out.println(25);
+        sc.close();
+    }
+}
